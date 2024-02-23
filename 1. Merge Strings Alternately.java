74a0cb1bd9a46ce2e54b998e8be390de14487a6a
saiprakashgorti <94886233+saiprakashgorti@users.noreply.github.com>
2024-02-23 20:02:07 +0530

    Update 1. Merge Strings Alternately.java

--- 1. Merge Strings Alternately.java
+++ 1. Merge Strings Alternately.java
@@ -9,6 +9,7 @@ Important details:
 
 **/
 
+// 1ms beats 85.41%
 class Solution {
     public String mergeAlternately(String word1, String word2) {
         int sizeOfWord1 = word1.length();
@@ -24,6 +25,33 @@ class Solution {
                 finalString.append(word1.charAt(indexForWord1++));
             if(indexForWord2 < sizeOfWord2)
                 finalString.append(word2.charAt(indexForWord2++));
+        }
+
+        return finalString.toString();
+    }
+}
+
+// 0ms beats 100%
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        int sizeOfWord1 = word1.length();
+        int sizeOfWord2 = word2.length();
+        int indexForWord1 = 0;
+        int indexForWord2 = 0;
+        int min = Math.min(sizeOfWord1, sizeOfWord2);
+        StringBuilder finalString = new StringBuilder();
+
+        for(int i=0; i<min; i++) {
+            finalString.append(word1.charAt(indexForWord1++));
+            finalString.append(word2.charAt(indexForWord2++));
+        }
+        
+        if(indexForWord1 < sizeOfWord1) {
+            finalString.append(word1.substring(indexForWord1));
+        }
+
+        if(indexForWord2 < sizeOfWord2) {
+            finalString.append(word2.substring(indexForWord2));
         }
 
         return finalString.toString();
